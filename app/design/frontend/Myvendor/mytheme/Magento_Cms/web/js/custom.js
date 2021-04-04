6778f8c4edf15f0a86f5feb81401b7056800bd2f
RamavtarSR <ramavtarrawat13@gmail.com>
2021-04-04 23:05:56 +0530

    added dots in carosul

--- app/design/frontend/Myvendor/mytheme/Magento_Cms/web/js/custom.js
+++ app/design/frontend/Myvendor/mytheme/Magento_Cms/web/js/custom.js
@@ -7,7 +7,7 @@
                     loop:true,
                     margin:20,
                     nav:true,
-                    dots:false,
+                    dots:true,
                     responsive:{
                         0:{
                             items:1
